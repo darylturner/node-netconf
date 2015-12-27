c0cc312ac56310a16d9851bbe8c8a943520e638b
Daryl Turner <daryl@ultramagnus.turner.private>
2015-12-27 12:35:25 +0000

    changed events to once

--- examples/pipeline.js
+++ examples/pipeline.js
@@ -2,9 +2,9 @@ module.exports.read = function (callback) {
     var data = '';
     process.stdin.once('data', function(chunk) {
         data += chunk;
-    }).on('end', function() {
+    }).once('end', function() {
         callback(null, data);
-    }).on('error', function(err) {
+    }).once('error', function(err) {
         callback(err, null);
     });
 };
