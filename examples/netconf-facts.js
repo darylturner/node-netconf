b989378ba3ccdedad55371c6a6de45b3e553a255
Daryl Turner <daryl@ultramagnus.turner.private>
2016-01-12 16:09:14 +0000

    show facts method usage

---
+++ examples/netconf-facts.js
@@ -0,0 +1,25 @@
+var netconf = require('../lib/netconf');
+var util = require('util');
+var fs = require('fs');
+
+function pprint(object) {
+    console.log(util.inspect(object, {depth:null, colors:true}));
+}
+
+var router = new netconf.Client({
+    host: '172.28.128.3',
+    username: 'vagrant',
+    pkey: fs.readFileSync('insecure_ssh.key', {encoding: 'utf8'})
+});
+
+router.open(function afterOpen(err) {
+    if (!err) {
+        router.facts(function (err, facts) {
+            router.close();
+            if (err) { throw (err); }
+            pprint(facts);
+        });
+    } else {
+        throw err;
+    }
+});
